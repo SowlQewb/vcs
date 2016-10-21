20dea9926a4094f1b439f8f43dabcb0dacf5a318
JR-PC\JR <jusram@gmail.com>
2016-10-21 22:10:47 +0300

    7 paskaita, final

---
+++ paskaita7/src/lt/vcs/PvzKlase.java
@@ -0,0 +1,19 @@
+package lt.vcs;
+
+import java.io.File;
+
+/**
+ * Created by JR on 2016.10.21.
+ */
+public class PvzKlase <T>{
+    private T reiksme;
+
+    public PvzKlase(T arg) {
+        reiksme = arg;
+    }
+
+    public T getReiksme() {
+        return reiksme;
+    }
+
+}
